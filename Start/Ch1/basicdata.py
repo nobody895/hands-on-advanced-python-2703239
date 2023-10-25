1722ddf51c982435331cedfc85d48f6b1d443d7f
nobody895 <52776039+nobody895@users.noreply.github.com>
2023-10-25 08:16:46 +0000

    changes until reduce function, include chapter 1 too

--- Start/Ch1/basicdata.py
+++ Start/Ch1/basicdata.py
@@ -9,10 +9,17 @@ with open("../../sample-weather-history.json", "r") as weatherfile:
     weatherdata = json.load(weatherfile)
 
 # TODO: What was the warmest day in the data set?
+warmday = max(weatherdata, key=lambda x: x['tmax'])
+print(f"warmest day = {warmday['date']}, at {warmday['tmax']} degrees")
+
 
 
 # TODO: What was the coldest day in the data set?
-
+coldday = min(weatherdata, key=lambda x: x['tmin'])
+print(f"coldest day = {coldday['date']}, at {coldday['tmin']} degrees")
 
 # TODO: How many days had snowfall?
+snow_days = [day for day in weatherdata if day['snow'] > 0]
+print(f"days with snow = {len(snow_days)}")
 
+pprint.pp(snow_days)
--- Start/Ch1/filteringdata.py
+++ Start/Ch1/filteringdata.py
@@ -10,12 +10,21 @@ with open("../../sample-weather-history.json", "r") as weatherfile:
 
 # the filter() function gives us a way to remove unwanted data points
 # TODO: create a subset of the data for days that had snowfall
-
+snowdays = list(filter(lambda x: x['snow'] > 0.0, weatherdata))
+# print(len(weatherdata))
+# print(len(snowdays))
 
 # TODO: pretty-print the resulting data set
-
+# pprint.pp(snowdays)
 
 # filter can also be used on non-numerical data, like strings
 # TODO: create a subset that contains summer days with heavy rain (more than 1 in, about 2.5cm)
 def is_summer_rain_day(d):
+    summer_months = ["-07-", "-08-"]
+    for sm in summer_months:
+        if sm in d['date'] and d['prcp'] >= 1:
+            return True
     return False
+
+summer_rain_days = list(filter(is_summer_rain_day, weatherdata))
+pprint.pp(summer_rain_days)
